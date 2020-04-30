674ff8a80d9f5c2db922ac904c95cde7404007cc
Pablo <pablo.alvesdebarros@iit.it>
2020-04-30 13:11:31 +0200

    Update Colab config

--- train_PK.py
+++ train_PK.py
@@ -7,11 +7,11 @@ config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 # sess = tf.Session(config=config)
 
-with tf.Session(config=config) as sess:
+with tf.compat.v1.Session(config=config) as sess:
     pk = PK.Model(sess)
 
     print ("-----------")
     print ("Initializing training!")
 
-    # with tf.device(device):
-    pk.train(num_epochs=2)
+    with tf.device(device):
+        pk.train(num_epochs=2)
